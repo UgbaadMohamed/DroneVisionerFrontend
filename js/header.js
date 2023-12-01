cc59acd3af552da9fdd5e071f9ad1361a53fb224
ikeaosman <ikhraabdiosman@gmail.com>
2023-12-01 12:20:06 +0100

    redirecting pricing works

--- js/header.js
+++ js/header.js
@@ -1,15 +1,14 @@
 document.addEventListener('DOMContentLoaded', function() {
     // This code will run after the DOM is fully loaded
-    fetch('/templates/header.html')
+    fetch('/templates/header.html')  // Assuming this is the correct path
         .then(response => response.text())
         .then(html => document.getElementById('header-placeholder').innerHTML = html);
+
 });
-
-
-    function navigateToPriser() {
-    window.location.href = '/templates/pricing.html';
+function navigateToPriser() {
+    window.location.href = 'pricing.html';
 }
 
-    function navigateToOmOs() {
-    window.location.href = '/templates/aboutUs.html';
+function navigateToOmOs() {
+    window.location.href = 'aboutUs.html';
 }
