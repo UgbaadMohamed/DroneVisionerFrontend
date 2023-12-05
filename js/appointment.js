db16e6c2f85f7350be623af707b9b6ea5539dce0
Ugbaad <ugbaad0012@gmail.com>
2023-12-05 18:46:29 +0100

    nyest udggave

--- js/appointment.js
+++ js/appointment.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
     postAppointmentToDatabase();
 });
 
-})
+
 
 //CALENDER --------------------------------------------------------------------------------------------
 
